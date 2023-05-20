dea1ab50b95434d924aa3babf29cfc419504fbc9
Alexander Vyzhymov <vizimov@gmail.com>
2023-05-21 00:51:27 +0300

    Updates

--- assets/index-3b7997a6.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./index-9f997953.js";const o=()=>e.jsx("h1",{children:"HomePage"}),r=o;export{r as default};
---
+++ assets/index-3fb5525b.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-a859e5c6.js";const o=()=>e.jsx("h1",{children:"HomePage"}),r=o;export{r as default};
