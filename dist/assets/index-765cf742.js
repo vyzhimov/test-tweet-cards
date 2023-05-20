85242f25dd031c2ac1c46ce9429d708436e7d7db
Alexander Vyzhymov <vizimov@gmail.com>
2023-05-21 00:15:55 +0300

    fixed-errors

---
+++ dist/assets/index-765cf742.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-2c029afa.js";const o=()=>e.jsx("h1",{children:"HomePage"}),r=o;export{r as default};
--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -4,7 +4,7 @@ import { lazy } from "react";
 import SharedLayout from "../SharedLayout";
 const Home = lazy(() => import("../../pages/Home"));
 const Tweets = lazy(() => import("../../pages/Tweets/"));
-const PageNotFound = lazy(() => "../PageNotFound/");
+import PageNotFound from "../PageNotFound/PageNotFound";
 
 function App() {
   return (
@@ -12,7 +12,7 @@ function App() {
       <Routes>
         <Route path="/" element={<SharedLayout />}>
           <Route index element={<Home />} />
-          <Route path="/tweets" element={<Tweets />} />
+          <Route path="tweets" element={<Tweets />} />
           <Route path="*" element={<PageNotFound />} />
         </Route>
       </Routes>
