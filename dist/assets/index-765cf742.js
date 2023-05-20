4f56d3336c034dce5ea4eda67e65e0f1e9b59574
Alexander Vyzhymov <vizimov@gmail.com>
2023-05-21 00:29:56 +0300

    fixed-errors

--- dist/assets/index-765cf742.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./index-2c029afa.js";const o=()=>e.jsx("h1",{children:"HomePage"}),r=o;export{r as default};
---
+++ dist/assets/index-cba6afc3.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-d2e7e7cc.js";const o=()=>e.jsx("h1",{children:"HomePage"}),r=o;export{r as default};
