6e70615633f31242ca773cd7629b712bc6b0149e
Alexander Vyzhymov <vizimov@gmail.com>
2023-05-21 09:20:56 +0300

    Updates

--- assets/index-77ecb299.js
+++
@@ -1,5 +0,0 @@
-import{n as s,j as e}from"./index-c5673372.js";const i=""+new URL("goitlogo-ee9d84e1.png",import.meta.url).href,o=""+new URL("movies-a4aeb14f.jpg",import.meta.url).href,t=""+new URL("nyt-242f343d.jpg",import.meta.url).href,r=""+new URL("shopgamers-ba2109f0.jpg",import.meta.url).href,m=s.img`
-  display: block;
-  margin: 0 auto;
-  width: 500px;
-`,h=()=>e.jsxs("div",{children:[e.jsx(m,{src:i,alt:"GoIT"}),e.jsxs("div",{children:[e.jsx("h1",{children:"My works:"}),e.jsxs("a",{href:"https://vyzhimov.github.io/goit-react-hw-05-movies/movies",children:[e.jsx("p",{children:"MOVIES"}),e.jsx("img",{src:o,alt:"movies"})]}),e.jsxs("a",{href:"https://vyzhimov.github.io/goit-react-hw-05-movies/movies",children:[e.jsx("p",{children:"NEWS PORTAL"}),e.jsx("img",{src:t,alt:"movies"})]}),e.jsxs("a",{href:"https://vyzhimov.github.io/goit-react-hw-05-movies/movies",children:[e.jsx("p",{children:"SHOP FOR GAMERS"}),e.jsx("img",{src:r,alt:"movies"})]})]})]}),a=h;export{a as default};
