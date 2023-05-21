1479d7ffb16a761dcc24e4ce7360bbe7f527b902
Alexander Vyzhymov <vizimov@gmail.com>
2023-05-21 09:20:43 +0300

    finished-home-page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,27 +3,42 @@ import Movie from "./movies.jpg";
 import News from "./nyt.jpg";
 import GamersShop from "./shopgamers.jpg";
 
-import { GoIT } from "./Home.styled";
+import { GoIT, Title, WorkList, WorksListItem } from "./Home.styled";
 
 const Home = () => {
   return (
     <div>
       <GoIT src={GoItImg} alt="GoIT" />
-      <div>
-        <h1>My works:</h1>
-        <a href="https://vyzhimov.github.io/goit-react-hw-05-movies/movies">
-          <p>MOVIES</p>
-          <img src={Movie} alt="movies"></img>
-        </a>
-        <a href="https://vyzhimov.github.io/goit-react-hw-05-movies/movies">
-          <p>NEWS PORTAL</p>
-          <img src={News} alt="movies"></img>
-        </a>
-        <a href="https://vyzhimov.github.io/goit-react-hw-05-movies/movies">
-          <p>SHOP FOR GAMERS</p>
-          <img src={GamersShop} alt="movies"></img>
-        </a>
-      </div>
+      <Title>MY WORKS</Title>
+      <WorkList>
+        <WorksListItem>
+          <a
+            href="https://vyzhimov.github.io/goit-react-hw-05-movies"
+            target="_blank"
+          >
+            <p>MOVIES</p>
+            <img src={Movie} alt="movies"></img>
+          </a>
+        </WorksListItem>
+        <WorksListItem>
+          <a
+            href="https://olgamunaieva.github.io/news-project-9/"
+            target="_blank"
+          >
+            <p>NEWS PORTAL</p>
+            <img src={News} alt="movies"></img>
+          </a>
+        </WorksListItem>
+        <WorksListItem>
+          <a
+            href="https://vyzhimov.github.io/project11-shopforgamers/"
+            target="_blank"
+          >
+            <p>SHOP FOR GAMERS</p>
+            <img src={GamersShop} alt="movies"></img>
+          </a>
+        </WorksListItem>
+      </WorkList>
     </div>
   );
 };
--- src/pages/Home/Home.styled.jsx
+++ src/pages/Home/Home.styled.jsx
@@ -3,5 +3,64 @@ import styled from "@emotion/styled";
 export const GoIT = styled.img`
   display: block;
   margin: 0 auto;
-  width: 500px;
+  width: 900px;
+  transition: animation 500ms ease-in-out;
+  animation: radar 2000ms ease infinite;
+
+  @keyframes radar {
+    0% {
+      -webkit-transform: scale(1, 1);
+      transform: scale(1, 1);
+    }
+    50% {
+      -webkit-transform: scale(1.05, 1.05);
+      transform: scale(1.05, 1.05);
+    }
+    100% {
+      -webkit-transform: scale(1, 1);
+      transform: scale(1, 1);
+    }
+  }
+`;
+
+export const Title = styled.h1`
+  text-align: center;
+`;
+
+export const WorkList = styled.ul`
+  margin-top: 30px;
+  display: flex;
+  gap: 30px;
+`;
+
+export const WorksListItem = styled.li`
+  position: relative;
+  width: calc((100% - 60px) / 3);
+  border-radius: 10px;
+  overflow: hidden;
+  box-shadow: -2.5777px 6.87386px 20.6216px rgba(0, 0, 0, 0.23);
+  transition: transform 250ms ease-in-out;
+
+  &:hover,
+  &:focus {
+    transform: scale(1.05);
+    -webkit-box-shadow: -1px 0px 12px 1px rgba(255, 255, 255, 1);
+    -moz-box-shadow: -1px 0px 12px 1px rgba(255, 255, 255, 1);
+    box-shadow: -1px 0px 12px 1px rgba(255, 255, 255, 1);
+  }
+
+  & img {
+    width: 100%;
+    height: 100%;
+  }
+
+  & p {
+    position: absolute;
+    background: rgba(75, 41, 153, 0.5);
+    color: #fff;
+    right: 0;
+    font-size: 25px;
+    padding: 10px;
+    border-bottom-left-radius: 5px;
+  }
 `;
